8d744e03bea4a996d3a08f35a16bdc10df5cce91
TuanVo3423 <tuanvanvo2003@gmail.com>
2023-12-10 20:07:39 +0700

    feat: fix socket

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-import express from 'express'
-import { config } from 'dotenv'
-import cors from 'cors'
+import bodyParser from 'body-parser'
 import { v2 as cloudinary } from 'cloudinary'
-config()
-import { Server } from 'socket.io'
+import cors from 'cors'
+import { config } from 'dotenv'
+import express from 'express'
+import { Server, Socket } from 'socket.io'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import instructionsRouter from './routes/instructions.routes'
+import likesRouter from './routes/likes.routes'
+import messagesRouter from './routes/messages.routes'
+import tweetsRouter from './routes/tweets.routes'
 import usersRouter from './routes/users.routes'
 import databaseServices from './services/database.services'
-import instructionsRouter from './routes/instructions.routes'
-import tweetsRouter from './routes/tweets.routes'
-import likesRouter from './routes/likes.routes'
-import bodyParser from 'body-parser'
-import messagesRouter from './routes/messages.routes'
-import { handleSocket } from './services/socket.services'
+config()
 
 databaseServices.connect()
 const app = express()
-const port = 3000
+const port = 5000
 app.use(cors({ origin: 'http://localhost:3001', credentials: true }))
 app.use(bodyParser.json({ limit: '30mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }))
@@ -39,8 +38,52 @@ const server = app.listen(port, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: '*'
+    origin: 'http://localhost:3001',
+    credentials: true
   }
 })
 
-io.on('connection', handleSocket)
+io.on('connection', (socket: Socket) => {
+  const listUsers: Array<any> = []
+  socket.on('joinChat', (user_id: string, target_user_id: string, name_nguoi_gui: string) => {
+    const user = { user_id: user_id, room: target_user_id, name_nguoi_gui: name_nguoi_gui }
+
+    if (listUsers.length === 0) {
+      // console.log('when list empty: ', user)
+      listUsers.push(user)
+    } else {
+      const isExist = listUsers.some((item) => item.user_id === user.user_id)
+      if (isExist) return
+      else {
+        listUsers.push(user)
+      }
+    }
+    // Join the room
+    listUsers.forEach((user) => {
+      socket.join(user.user_id)
+    })
+    // console.log('Joined rooms:', socket.rooms)
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('createChat', async (msg) => {
+    const { user_recieved_id } = msg
+
+    // Kiểm tra nếu user_recieved_id trùng với socket.id của người gửi
+    if (user_recieved_id === socket.id) {
+      console.log('Tin nhắn không được gửi cho chính mình.')
+      return
+    }
+    // console.log(`${name} đã gửi tin nhắn đến ${user_recieved_id}}`)
+    socket.to(user_recieved_id).emit('sendChatToClient', msg)
+  })
+})
+// io.use((socket, next) => {
+//   const username = socket.handshake.auth.username
+//   if (!username) {
+//     return next(new Error('invalid username'))
+//   }
+//   socket.username = username
+//   next()
+// })
--- src/services/socket.services.ts
+++ src/services/socket.services.ts
@@ -1,16 +1,14 @@
 import { Socket } from 'socket.io'
 const listUsers: Array<any> = []
 export const handleSocket = (socket: Socket) => {
-  socket.on('joinChat', (user_id: string, target_user_id: string) => {
-    const user = { user_id: user_id, room: target_user_id }
-    // fisrt time
-    console.log('user: ', user)
+  socket.on('joinChat', (user_id: string, target_user_id: string, name_nguoi_gui: string) => {
+    const user = { user_id: user_id, room: target_user_id, name_nguoi_gui: name_nguoi_gui }
 
     if (listUsers.length === 0) {
+      console.log('when list empty: ', user)
       listUsers.push(user)
     } else {
       const isExist = listUsers.some((item) => item.user_id === user.user_id)
-      console.log('isExist: ', isExist)
       if (isExist) return
       else {
         listUsers.push(user)
@@ -22,14 +20,18 @@ export const handleSocket = (socket: Socket) => {
     })
     console.log('Joined rooms:', socket.rooms)
   })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
   socket.on('createChat', async (msg) => {
     const { user_id, content, name, created_at, user_recieved_id } = msg
-    console.log('gui toi thang nay: ', user_recieved_id)
+
     // Kiểm tra nếu user_recieved_id trùng với socket.id của người gửi
     if (user_recieved_id === socket.id) {
       console.log('Tin nhắn không được gửi cho chính mình.')
       return
     }
+    console.log(`${name} đã gửi tin nhắn đến ${user_recieved_id}}`)
     socket.to(user_recieved_id).emit('sendChatToClient', msg)
   })
 }
